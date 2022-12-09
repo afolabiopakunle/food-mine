4ac82b11ae193691ad3ed4982f09be1b89dc0e85
Afolabi <opakunleafolabi@gmail.com>
2022-12-09 12:14:27 +0100

    food card implemented

---
+++ front-end/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from '../../../shared/models/Food';
+import { FoodService } from '../../../services/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods: Food[] = [];
+
+  constructor(private foodService: FoodService) { }
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAllFood();
+  }
+
+}
