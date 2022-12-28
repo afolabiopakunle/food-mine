b73ef52bfa5c0fb1e11dcf1a75051f718abe2d3a
Afolabi <opakunleafolabi@gmail.com>
2022-12-29 00:48:35 +0100

    url endpoints created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods } from './data';
+import { sample_foods, sample_tags } from './data';
 
 const app = express();
 
@@ -10,9 +10,29 @@ app.use(cors({
 }));
 
 app.get("/api/foods", (req, res) => {
-    res.send(sample_foods)
+    res.send(sample_foods);
 });
 
-const PORT = 5000;
+app.get("/api/foods/search/:searchTerm", (req, res) => {
+    const searchTerm = req.params.searchTerm;
+   res.send(sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase())));
+});
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.get("/api/foods/tags", (req, res) => {
+    res.send(sample_tags);
+});
+
+app.get("/api/foods/tag/:tagName", (req, res) => {
+    const searchTag = req.params.tagName;
+    res.send(searchTag === 'All' ? sample_foods : sample_foods.filter(food => food.tags?.map((tag: string) => tag.toLowerCase()).includes(searchTag.toLowerCase())));
+});
+
+app.get("/api/foods/:foodId", (req, res) => {
+    const foodId = req.params.foodId
+    const food = sample_foods.find(food => food.id == foodId);
+    res.send(food);
+})
+
+const PORT = 5001;
+
+app.listen(PORT, () => console.log(`App listening on high port ${PORT}`));
