f22a1120c1e2c4ceabd8f5adb0b01cbc864113ac
Afolabi <opakunleafolabi@gmail.com>
2023-01-08 19:04:57 +0100

    login form ts implemented

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodCardComponent } from './components/partials/food-card/food-card.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
@@ -15,6 +15,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundPageComponent } from './components/partials/not-found-page/not-found-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     CartPageComponent,
     TitleComponent,
     NotFoundPageComponent,
+    LoginComponent,
 
   ],
   imports: [
@@ -36,6 +38,7 @@ import { HttpClientModule } from '@angular/common/http';
     RatingModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  isSubmitted = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if(this.loginForm.invalid) return;
+    alert(`email: ${this.fc['email'].value},
+    password: ${this.fc['password'].value}`)
+  }
+}
