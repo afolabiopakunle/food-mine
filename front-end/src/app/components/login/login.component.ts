eac597f214a684f56f5d1e673cdcc68036a1d0ab
Afolabi <opakunleafolabi@gmail.com>
2023-01-13 14:16:55 +0100

    login form errors implemented

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
   }
 
@@ -26,8 +26,10 @@ export class LoginComponent implements OnInit {
 
   submit() {
     this.isSubmitted = true;
-    if(this.loginForm.invalid) return;
+    if(this.loginForm.invalid) {
+      return
+    } else {
     alert(`email: ${this.fc['email'].value},
     password: ${this.fc['password'].value}`)
-  }
+  }}
 }
